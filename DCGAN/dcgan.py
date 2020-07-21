5dc6eed6fec3f8dfa9dd561fba6efb3737c37b8e
Harsha <ashharsha1999@gmail.com>
2020-07-21 22:52:52 +0530

    Fixed errors in Model

--- DCGAN/dcgan.py
+++ DCGAN/dcgan.py
@@ -3,32 +3,32 @@ import  torch.nn as nn
 
 
 class Generator(nn.Module):
-    def __init__(self,out_dims=3, ,zdims):
+    def __init__(self, zdims,out_dims=1):
         super().__init__()
 
         self.gen = nn.Sequential(
                                                         ## Block 1
-                                                        nn.ConvTranspose2d(zdims, 1024, kernel_size=4, bias=False),
+                                                        nn.ConvTranspose2d(zdims, 1024, kernel_size=4, stride = 1,padding =0, bias=False),
                                                         nn.BatchNorm2d(1024),
                                                         nn.ReLU(inplace=True),
 
                                                         ## Block 2
-                                                        nn.ConvTranspose2d(1024, 512, kernel_size=8, , bias=False),
+                                                        nn.ConvTranspose2d(1024, 512, kernel_size=8, stride = 2,padding =1, bias=False),
                                                         nn.BatchNorm2d(512),
                                                         nn.ReLU(inplace=True),
 
                                                         ## Block 3
-                                                        nn.ConvTranspose2d(512, 256, kernel_size=16, , bias=False),
+                                                        nn.ConvTranspose2d(512, 256, kernel_size=16, stride = 2,padding =1, bias=False),
                                                         nn.BatchNorm2d(256),
                                                         nn.ReLU(inplace=True),
 
                                                         ## Block 4
-                                                        nn.ConvTranspose2d(256, 128, kernel_size=32, , bias=False),
+                                                        nn.ConvTranspose2d(256, 128, kernel_size=32, stride = 2,padding =1, bias=False),
                                                         nn.BatchNorm2d(128),
                                                         nn.ReLU(inplace=True),
                                                         
                                                         ## Output Layer
-                                                        nn.ConvTranspose2d(128, out_dims, kernel_size=64, , bias=False),
+                                                        nn.ConvTranspose2d(128, out_dims, kernel_size=64, stride = 2,padding =1, bias=False),
                                                         nn.Tanh()
         )
     
@@ -38,37 +38,36 @@ class Generator(nn.Module):
 
 class Discriminator(nn.Module):
 
-    def __init__(self):
+    def __init__(self, inp_dims):
 
         super().__init__()
 
         self.dis = nn.Sequential(
                                                         ## Block 1
-                                                        nn.Conv2d(inp_dims,64, kernel_size = 4, stride =1, padding = 1, bias = False)
+                                                        nn.Conv2d(inp_dims,64, kernel_size = 4, stride =2, padding = 1, bias = False),
                                                         nn.LeakyReLU(0.2,inplace=True),
 
                                                         ## Block 2
-                                                        nn.Conv2d(64,128, kernel_size = 4, stride =1, padding = 1, bias = False),
+                                                        nn.Conv2d(64,128, kernel_size = 4, stride =2, padding = 1, bias = False),
                                                         nn.BatchNorm2d(128),
                                                         nn.LeakyReLU(0.2,inplace=True),
 
                                                         ## Block 3
-                                                        nn.Conv2d(128,256, kernel_size = 4, stride =1, padding = 1, bias = False),
+                                                        nn.Conv2d(128,256, kernel_size = 4, stride =2, padding = 1, bias = False),
                                                         nn.BatchNorm2d(256),
                                                         nn.LeakyReLU(0.2,inplace=True),
 
                                                         ## Block 4
-                                                        nn.Conv2d(256,512, kernel_size = 4, stride =1, padding = 0, bias = False),
+                                                        nn.Conv2d(256,512, kernel_size = 4, stride =2, padding = 1, bias = False),
                                                         nn.BatchNorm2d(512),
                                                         nn.LeakyReLU(0.2,inplace=True),
 
                                                         ## Output Layer
-                                                        nn.Conv2d(512, 1, bias = False),
+                                                        nn.Conv2d(512,1, kernel_size = 4, stride =1, padding = 0, bias = False),
                                                         nn.Sigmoid()
         )
 
     def forward(self, x):
-
         return self.dis(x)
 
 ## Function to initializa weights
